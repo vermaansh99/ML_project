3ad8a2a91ea704a61a33841088945a1fb18a5109
vermaansh99 <vermaansh99@gmail.com>
2023-05-03 00:55:30 +0530

    notebook added

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exe_info()
@@ -21,6 +20,9 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
+
     
 
     
